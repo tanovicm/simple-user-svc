abeb6714074b6298f26cbcd1f004a83ce02891ed
Marijana Tanovic <tanulica7@gmail.com>
2022-02-06 22:45:07 +0100

    rm main, test.sh + updated docker-compose.yml

--- test.sh
+++
@@ -1,3 +0,0 @@
-curl -XPOST -d '{"firstname":"Marijana","lastname":"Tanovic","nickname":"Mara","password":"123","email":"mara@mara","country":"Srbija"}' 'localhost:8000/users'
-curl -XGET 'localhost:8000/users/61ffb1934697be8880d8b843'
-curl -XDELETE 'localhost:8000/users/61ffb1934697be8880d8b843'
